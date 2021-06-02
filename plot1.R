ce76c14b69b4d23a0328ca54a7a13c3241e01035
Vincent Frenz <46810338+vincentfrenz@users.noreply.github.com>
2021-06-02 10:56:37 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,20 +1,18 @@
-# load packages
+## Plot 1 - RScript
+
+## load packages
 library(dplyr)
-library(lubridate)
 
 ## set directory
-setwd("~/GitHub/CourseProject1")
+setwd("~/GitHub/ExData_Plotting1")
 
 ## load data
 myData <- read.csv2("./exdata_data_household_power_consumption/household_power_consumption.txt", header = TRUE, dec = ".", na.strings = "?")
 
-# filter for relevant time period
+## filter for relevant time period
 TwoDaysData <- filter(myData, c(Date == "1/2/2007" | Date == "2/2/2007"))
 
-TwoDaysData$Date = as.Date(TwoDaysData$Date, "%d/%m/%Y")
+hist(TwoDaysData$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
 
-TwoDaysDataWeekdays <- weekdays(TwoDaysData$Date)
-
-class(myData$Date)
-?filter
-
+dev.copy(png, file = "plot 1.png")
+dev.off()
