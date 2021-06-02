71fb74eee6deeb3aebfec964db5dfdc7b8d7d82c
Vincent Frenz <46810338+vincentfrenz@users.noreply.github.com>
2021-06-02 11:43:09 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -12,7 +12,9 @@ myData <- read.csv2("./exdata_data_household_power_consumption/household_power_c
 ## filter for relevant time period
 TwoDaysData <- filter(myData, c(Date == "1/2/2007" | Date == "2/2/2007"))
 
+## plot
 hist(TwoDaysData$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
 
+## png
 dev.copy(png, file = "plot1.png")
 dev.off()
